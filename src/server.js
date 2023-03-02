b82282e29ecf3948a8295344d4ee70e1b6796a53
zuoboo <naoya.saiki@rightcode.co.jp>
2023-03-02 18:26:33 +0900

    prismaクライアント接続

--- src/server.js
+++ src/server.js
@@ -1,36 +1,27 @@
 const { ApolloServer, gql, delegateToSchema } = require("apollo-server");
-const fs = require("fs")
-const path = require("path")
+const fs = require("fs");
+const path = require("path");
 
-
-// HackerNewsの１つ１つの投稿
-let links = [
-  {
-    id: "link-0",
-    description: "GraphQLチュートリアル学習中",
-    url: "www.graphql-tutorial.com",
-  },
-];
-
-
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 //リゾルバ関数
 const resolvers = {
   Query: {
     info: () => "HackerNewsクローン",
-    feed: () => links,
+    feed: async (parent, args, context) => {
+      return context.prisma.link.findMany();
+    },
   },
   Mutation: {
-    post: (parent, args) => {
-      let idCount = links.length;
-
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url,
-      };
-      links.push(link);
-      return link;
+    post: (parent, args, context) => {
+      const newLink = context.prisma.link.create({
+        data: {
+          url: args.url,
+          description: args.description,
+        },
+      });
+      return newLink
     },
   },
 };
@@ -38,6 +29,9 @@ const resolvers = {
 const server = new ApolloServer({
   typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf-8"),
   resolvers,
+  context: {
+    prisma,
+  },
 });
 
 server
